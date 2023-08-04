266c0905feb0baffe96b7056c7d7a42d0968fca9
Alfredo Aguiar Arce <67090401+alfredoaguiararce@users.noreply.github.com>
2023-08-04 00:12:41 -0700

    :zap: Fix issue with json file

--- app/Program.cs
+++ app/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using app.models;
+using app.Services;
+
+LinqQueries queries = new LinqQueries();
+
+PrintAll(queries.GetBooks());
+
+void PrintAll(IEnumerable<Book> listadelibros)
+{
+    Console.WriteLine("{0,-60} {1, 15} {2, 15}\n", "Titulo", "N. Paginas", "Fecha publicacion");
+    foreach (var item in listadelibros)
+    {
+        Console.WriteLine("{0,-60} {1, 15} {2, 15}", item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
+    }
+}
